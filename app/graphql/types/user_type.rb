ecd56dd106b64d117293e9d0613fb5615e75d877
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-15 12:50:15 +0900

    rm duplications

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,7 +4,5 @@ module Types
     field :name, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :id, ID, null: false
-    field :name, String, null: false
   end
 end

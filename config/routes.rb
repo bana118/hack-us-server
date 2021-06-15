38de91905f144918806c1e6841d451416bb93e1d
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-15 12:57:59 +0900

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  if Rails.env.development?
+    # add the url of your end-point to graphql_path.
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" 
+  end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

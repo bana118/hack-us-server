d01fe9d48aa1cd44d657fe5f28661906d1ebd680
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-19 12:04:15 +0900

    create Paticipants table

---
+++ db/migrate/20210619033786_create_participants.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateParticipants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :participants do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

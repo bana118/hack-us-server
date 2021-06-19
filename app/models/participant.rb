902274267517727e303d2b17b94bc1d22b6442d1
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-19 20:01:35 +0900

    add uniqueness validation to user-project

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,4 +3,11 @@
 class Participant < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  # ユーザは同一のプロジェクトに一度しか参加できない
+  validates :user,
+            uniqueness: {
+              scope: :project_id,
+              message: 'this user already participated to this project'
+            }
 end

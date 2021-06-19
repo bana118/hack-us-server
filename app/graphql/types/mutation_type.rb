2a06b5d4e763baa5773b951fdd56dae67d8871f7
RenFukatsu <fukatsuren88@gmail.com>
2021-06-19 13:53:15 +0000

    fix lint

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
-    field :create_project, mutation: Mutations::CreateProject
+    field :createProject, mutation: Mutations::CreateProject
     field :createUser, mutation: Mutations::CreateUser
   end
 end

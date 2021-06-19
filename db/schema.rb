a8747e4e0b6391ecbdf81a00562724c0a39ebd38
RenFukatsu <fukatsuren88@gmail.com>
2021-06-19 07:18:29 +0000

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_153304) do
+ActiveRecord::Schema.define(version: 2021_06_17_145838) do
 
   create_table "projects", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,9 +21,10 @@ ActiveRecord::Schema.define(version: 2021_06_16_153304) do
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid", null: false
   end
 
 end

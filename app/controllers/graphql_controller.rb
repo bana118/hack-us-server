fb4ce13b71cb90404d12653dcc289b536202e579
bana118 <bana.titech@gmail.com>
2021-06-19 11:01:58 +0000

    receive token as authorization header

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -12,7 +12,7 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user,
     }
     result = HackUsServerSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -47,5 +47,13 @@ class GraphqlController < ApplicationController
       logger.error e.backtrace.join("\n")
 
       render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
+    end
+
+    def current_user
+      puts "HTTP_AUTHORIZATION:"
+      puts request.headers["HTTP_AUTHORIZATION"]
+      return nil if request.headers["Authorization"].blank?
+      token = request.headers["Authorization"].split(" ").last
+      return nil if token.blank?
     end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :options]
+      methods: [:get, :post, :options],
+      # TODO クライアントとのクッキー共有のための設定だがクッキー共有ができていないので不要かもしれない
+      credentials: true
   end
 end

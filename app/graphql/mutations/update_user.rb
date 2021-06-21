863fc934d18ee715bc9cebac1cbca25defc4e2e6
bana118 <bana.titech@gmail.com>
2021-06-21 15:54:37 +0000

    modify updateUser mutation

--- app/graphql/mutations/update_user.rb
+++ app/graphql/mutations/update_user.rb
@@ -8,13 +8,12 @@ module Mutations
     field :result, Boolean, null: true
 
     argument :uid, String, required: true
-    argument :name, String
-    argument :description, String
-    argument :github_id, String
+    argument :name, String, required: false
+    argument :description, String, required: false
 
-    def resolve(uid:, name:, description:, github_id:)
+    def resolve(uid:, name:, description:)
       raise GraphQL::ExecutionError, "Authentication failed" if context[:current_user].nil? || context[:current_user]["uid"] != uid
-      arg = { name: name, description: description, github_id: github_id }
+      arg = { name: name, description: description }
       updates = arg.compact
       user = User.find_by(uid: uid)
       user.update(updates)

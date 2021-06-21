ed9ba24d17271e122630f58560dcd6336daf8c00
bana118 <bana.titech@gmail.com>
2021-06-21 09:56:35 +0000

    modiry query and mutation type for create user

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -7,16 +7,17 @@ module Mutations
     field :user, Types::UserType, null: false
     field :result, Boolean, null: true
 
-    argument :name, String, required: true
     argument :uid, String, required: true
+    argument :github_id, String, required: true
 
-    def resolve(name:, uid:)
+    def resolve(uid:, github_id:)
       raise GraphQL::ExecutionError, "Authentication failed" if context[:current_user].nil? || context[:current_user]["uid"] != uid
-
-      user = User.create(name: name, uid: uid)
+      name =  context[:current_user]["decoded_token"][:payload]["name"]
+      picture = context[:current_user]["decoded_token"][:payload]["picture"]
+      user = User.create(name: name, uid: uid, description: "", github_id: github_id, github_icon_url: picture)
       {
         user: user,
-        result: user.errors.blank?
+        result: user.errors.blank?,
       }
     end
   end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,10 +4,10 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
-    field :uid, String, null: false,
-    field :description, String, null: false,
-    field :github_id, String, null: false,
-    field :github_icon_url, String, null: false,
+    field :uid, String, null: false
+    field :description, String, null: false
+    field :github_id, String, null: false
+    field :github_icon_url, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end

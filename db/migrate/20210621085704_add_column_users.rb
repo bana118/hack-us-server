6abefbe3e65795294e37df5bda90925bbdcc11af
bana118 <bana.titech@gmail.com>
2021-06-21 09:04:30 +0000

    add github_id column to users and modify query user type

--- db/migrate/20210621085704_add_column_users.rb
+++ db/migrate/20210621085704_add_column_users.rb
@@ -1,6 +1,7 @@
 class AddColumnUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :description, :string, null: false
+    add_column :users, :github_id, :string, null: false
     add_column :users, :github_icon_url, :string, null: false
   end
 end

28370117a2b7d63097d90dd134aa24c7b78ca8af
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-22 20:25:58 +0900

    add InputArgument to createProject

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -7,5 +7,17 @@ module Types
     field :name, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :description, String, null: true
+    field :github_url, String, null: true
+    field :starts_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :ends_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :technology1, String, null: true
+    field :technology2, String, null: true
+    field :technology3, String, null: true
+    field :technology4, String, null: true
+    field :technology5, String, null: true
+    field :recruitment_numbers, Int, null: true
+    field :tool_link, String, null: true
+    field :contribution, String, null: true
   end
 end

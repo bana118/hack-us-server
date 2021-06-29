8803d339467a30372871c348f26163f444d05f97
bana118 <bana.titech@gmail.com>
2021-06-29 23:05:38 +0900

    add has many option to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,5 +4,5 @@ class Project < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: :owner_uid, primary_key: :uid
   has_many :participants
   has_many :favorites
-  has_many :languages
+  has_many :languages, -> { order("name DESC") }
 end
